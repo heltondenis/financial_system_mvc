e6d4d3b0ca997370ee06791168c71022dc546350
Helton Denis <heltondeniss@gmail.com>
2018-07-26 22:29:54 -0300

    view permission_add

---
+++ views/permissions_add.php
@@ -0,0 +1,7 @@
+<h1>Permissions - Adicionar</h1>
+
+<form method="POST">
+	<label for="label">Nome da Permissão</label>
+	<input type="text" name="name" /><br><br>
+	<input type="submit" value="Adicionar" />
+</form>
